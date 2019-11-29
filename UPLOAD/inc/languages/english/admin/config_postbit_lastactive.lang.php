888b70bacfbf19894bb9b2a64f85c081f29a937e
Sven <SvePu@users.noreply.github.com>
2019-11-29 13:37:44 +0100

    Fixes for #5

--- UPLOAD/inc/languages/english/admin/config_postbit_lastactive.lang.php
+++ UPLOAD/inc/languages/english/admin/config_postbit_lastactive.lang.php
@@ -9,17 +9,28 @@
 $l['pla_plugin_name'] = 'User Last Active Time in Postbit';
 $l['pla_plugin_desc'] = 'Simple MyBB 1.8 plugin to show the last active time of user in postbit details';
 $l['pla_settings_title'] = 'User Last Active Time Settings';
+
 $l['pla_settings_title_desc'] = 'Settings for the User Last Active Time plugin';
+
 $l['pla_enable_title'] = 'Do you want enabe this feature?';
 $l['pla_enable_title_desc'] = 'Set YES to enable it!';
+
 $l['pla_groupselect_title'] = 'User groups which can see the last user active time in postbits.';
 $l['pla_groupselect_desc'] = 'Select usergroups.';
+
 $l['pla_timeformat_title'] = 'Date/Timeformat';
 $l['pla_timeformat_desc'] = 'Set the desired date and time format (For more informations visit <a href="http://php.net/manual/en/function.date.php" target="_blank">php.net</a>).';
+
 $l['pla_timeformat_default'] = 'Y-m-d, g:i a';
+
 $l['pla_showonlinestatus_title'] = 'Show ONLINE for logged in Users';
 $l['pla_showonlinestatus_desc'] = 'Set YES to enable it!';
+
+$l['pla_onlinecutofftime_title'] = 'Online CutOff Time';
+$l['pla_onlinecutofftime_desc'] = 'Set number of minutes after a logged in user is marked as inactive (default: 5)';
+
 $l['pla_onlinestatus_text_title'] = 'ONLINE Text String';
 $l['pla_onlinestatus_text_desc'] = 'Set the text which should be displayed while user is ONLINE';
+
 $l['pla_onlinestatus_style_title'] = 'ONLINE CSS Inline Style';
 $l['pla_onlinestatus_style_desc'] = 'Set some CSS inline stylesheet for ONLINE text string';
--- UPLOAD/inc/plugins/postbit_lastactive.php
+++ UPLOAD/inc/plugins/postbit_lastactive.php
@@ -11,7 +11,8 @@ if(!defined("IN_MYBB"))
 {
     die("Direct initialization of this file is not allowed.");
 }
-
+$plugins->add_hook("admin_config_settings_change",'postbit_lastactive_settings_page');
+$plugins->add_hook("admin_page_output_footer",'postbit_lastactive_settings_peeker');
 $plugins->add_hook('postbit', 'postbit_lastactive_run');
 $plugins->add_hook('postbit_prev', 'postbit_lastactive_run');
 $plugins->add_hook('postbit_pm', 'postbit_lastactive_run');
@@ -27,40 +28,40 @@ function postbit_lastactive_info()
         "website"       => "https://github.com/SvePu/MyBB-Postbit-UserLastActiveTime",
         "author"        => "Svepu",
         "authorsite"    => "https://github.com/SvePu",
-        "version"       => "1.3",
+        "version"       => "1.4",
         "codename"      => "postbitlastactive",
         "compatibility" => "18*"
     );
-	
-	$info_desc = '';
-	$gid_result = $db->simple_select('settinggroups', 'gid', "name = 'pla_settings'", array('limit' => 1));
-	$settings_group = $db->fetch_array($gid_result);
-	if(!empty($settings_group['gid']))
-	{
-		$info_desc .= "<span style=\"font-size: 0.9em;\">(~<a href=\"index.php?module=config-settings&action=change&gid=".$settings_group['gid']."\"> ".htmlspecialchars_uni($lang->pla_settings_title)." </a>~)</span>";
-	}
-    
+
+    $info_desc = '';
+    $gid_result = $db->simple_select('settinggroups', 'gid', "name = 'pla_settings'", array('limit' => 1));
+    $settings_group = $db->fetch_array($gid_result);
+    if(!empty($settings_group['gid']))
+    {
+        $info_desc .= "<span style=\"font-size: 0.9em;\">(~<a href=\"index.php?module=config-settings&action=change&gid=".$settings_group['gid']."\"> ".htmlspecialchars_uni($lang->pla_settings_title)." </a>~)</span>";
+    }
+
     if(is_array($plugins_cache) && is_array($plugins_cache['active']) && $plugins_cache['active']['postbit_lastactive'])
     {
-		$info_desc .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;" target="_blank" />
+        $info_desc .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;" target="_blank" />
 <input type="hidden" name="cmd" value="_s-xclick" />
 <input type="hidden" name="hosted_button_id" value="VGQ4ZDT8M7WS2" />
 <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/btn/btn_donate_pp_142x27.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
 <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
 </form>';
-	}
-	
-	if($info_desc != '')
-	{
-		$info['description'] = $info_desc.'<br />'.$info['description'];
-	}
-    
+    }
+
+    if($info_desc != '')
+    {
+        $info['description'] = $info_desc.'<br />'.$info['description'];
+    }
+
     return $info;
 }
 
 function postbit_lastactive_activate()
 {
-	global $db, $mybb, $lang;
+    global $db, $mybb, $lang;
 
     $lang->load("config_postbit_lastactive");
     $query_add = $db->simple_select("settinggroups", "COUNT(*) as disporder");
@@ -100,22 +101,29 @@ function postbit_lastactive_activate()
             'title'         => $db->escape_string($lang->pla_showonlinestatus_title),
             'description'   => $db->escape_string($lang->pla_showonlinestatus_desc),
             'optionscode'   => 'yesno',
-            'value'         => 0,
+            'value'         => 1,
             "disporder"     => 4
+        ),
+        'pla_onlinecutofftime' => array(
+            'title'         => $db->escape_string($lang->pla_onlinecutofftime_title),
+            'description'   => $db->escape_string($lang->pla_onlinecutofftime_desc),
+            'optionscode'   => 'numeric',
+            'value'         => 5,
+            "disporder"     => 5
         ),
         'pla_onlinestatus_text' => array(
             'title'         => $db->escape_string($lang->pla_onlinestatus_text_title),
             'description'   => $db->escape_string($lang->pla_onlinestatus_text_desc),
             'optionscode'   => 'text',
             'value'         => 'Online',
-            "disporder"     => 5
+            "disporder"     => 6
         ),
         'pla_onlinestatus_style' => array(
             'title'         => $db->escape_string($lang->pla_onlinestatus_style_title),
             'description'   => $db->escape_string($lang->pla_onlinestatus_style_desc),
             'optionscode'   => 'textarea',
             'value'         => 'color:green;\nfont-weight:bold;',
-            "disporder"     => 6
+            "disporder"     => 7
         ),
     );
 
@@ -132,7 +140,7 @@ function postbit_lastactive_activate()
 
 function postbit_lastactive_deactivate()
 {
-	global $db;
+    global $db;
 
     $result = $db->simple_select('settinggroups', 'gid', "name = 'pla_settings'", array('limit' => 1));
     $pla_group = $db->fetch_array($result);
@@ -159,33 +167,76 @@ function postbit_lastactive_run(&$post)
     }
 
     $lang->load("postbit_lastactive");
-    $post['last_active_info'] = '';
+    $post['last_active_info'] = "";
 
-    if($post['lastvisit'] == $post['lastactive'])
+    if($post['invisible'] != 1 || $mybb->usergroup['canviewwolinvis'] == 1)
     {
-        if(empty($mybb->settings['pla_timeformat']))
+        if($mybb->settings['pla_showonlinestatus'] == 1 && $post['lastvisit'] != $post['lastactive'])
         {
-            $mybb->settings['pla_timeformat'] = "relative";
-        }
-        $lastactivetime = my_date($mybb->settings['pla_timeformat'], $post['lastactive']);
-        $post['last_active_info'] = '<br />'.$lang->sprintf(htmlspecialchars_uni($lang->pla_lastactive), $lastactivetime);
-    }
+            if($mybb->settings['pla_onlinecutofftime'] < 1)
+            {
+                $mybb->settings['pla_onlinecutofftime'] = 1;
+            }
+            $cutofftime = $mybb->settings['pla_onlinecutofftime']*60;
 
-    if($post['lastvisit'] != $post['lastactive'] && $mybb->settings['pla_showonlinestatus'] == "1")
-    {
-        if(empty($mybb->settings['pla_onlinestatus_text']))
-        {
-            $mybb->settings['pla_onlinestatus_text'] = "Online";
+            if($post['lastactive'] > TIME_NOW - $cutofftime)
+            {
+                if(empty($mybb->settings['pla_onlinestatus_text']))
+                {
+                    $mybb->settings['pla_onlinestatus_text'] = "Online";
+                }
+
+                $onlinecssstyle = '';
+                if(!empty($mybb->settings['pla_onlinestatus_style']))
+                {
+                    $mybb->settings['pla_onlinestatus_style'] = preg_replace("%(\r\n)|(\r)%", "", $mybb->settings['pla_onlinestatus_style']);
+                    $onlinecssstyle = 'style="' . $mybb->settings['pla_onlinestatus_style'] . '"';
+                }
+                $post['last_active_info'] = '<br /><span '.$onlinecssstyle.'>' . $mybb->settings['pla_onlinestatus_text'] . '</span>';
+            }
+            else
+            {
+                if(empty($mybb->settings['pla_timeformat']))
+                {
+                    $mybb->settings['pla_timeformat'] = "relative";
+                }
+                $lastactivetime = my_date($mybb->settings['pla_timeformat'], ($post['lastactive'] + $cutofftime));
+                $post['last_active_info'] = '<br />'.$lang->sprintf(htmlspecialchars_uni($lang->pla_lastactive), $lastactivetime);
+            }
         }
 
-        $onlinecssstyle = '';
-        if(!empty($mybb->settings['pla_onlinestatus_style']))
+        if($post['lastvisit'] == $post['lastactive'])
         {
-            $mybb->settings['pla_onlinestatus_style'] = preg_replace("%(\r\n)|(\r)%", "", $mybb->settings['pla_onlinestatus_style']);
-            $onlinecssstyle = 'style="' . $mybb->settings['pla_onlinestatus_style'] . '"';
+            if(empty($mybb->settings['pla_timeformat']))
+            {
+                $mybb->settings['pla_timeformat'] = "relative";
+            }
+            $lastactivetime = my_date($mybb->settings['pla_timeformat'], $post['lastactive']);
+            $post['last_active_info'] = '<br />'.$lang->sprintf(htmlspecialchars_uni($lang->pla_lastactive), $lastactivetime);
         }
-        $post['last_active_info'] = '<br /><span '.$onlinecssstyle.'>' . $mybb->settings['pla_onlinestatus_text'] . '</span>';
     }
 
     $post['user_details'] = $post['user_details'].$post['last_active_info'];
+}
+
+function postbit_lastactive_settings_page()
+{
+    global $db, $mybb, $pla_settings_peeker;
+    $result = $db->simple_select('settinggroups', 'gid', "name = 'pla_settings'", array('limit' => 1));
+    $pla_group = $db->fetch_array($result);
+    $pla_settings_peeker = ($mybb->input["gid"] == $pla_group["gid"]) && ($mybb->request_method != "post");
+}
+
+function postbit_lastactive_settings_peeker()
+{
+    global $pla_settings_peeker;
+    if($pla_settings_peeker)
+    {
+        echo '<script type="text/javascript">
+        $(document).ready(function(){
+            new Peeker($(".setting_pla_enable"), $("#row_setting_pla_groupselect, #row_setting_pla_timeformat, #row_setting_pla_showonlinestatus, #row_setting_pla_onlinecutofftime, #row_setting_pla_onlinestatus_text, #row_setting_pla_onlinestatus_style"), 1, true),
+            new Peeker($(".setting_pla_showonlinestatus"), $("#row_setting_pla_onlinecutofftime, #row_setting_pla_onlinestatus_text, #row_setting_pla_onlinestatus_style"), 1, true)
+        });
+        </script>';
+    }
 }
